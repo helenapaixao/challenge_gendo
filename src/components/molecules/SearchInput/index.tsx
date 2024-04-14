abe3efc52b947d3f5fd59a9f082977462f6f5e1f
helenapaixao <hp.helenapaixao@gmail.com>
2024-04-13 21:59:11 -0300

    [feat] Create SearchInput component with FontAwesomeIcon and styled input

---
+++ src/components/molecules/SearchInput/index.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import { Container, IconContainer, StyledInput } from "./styles";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+
+interface SearchInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  placeholder: string;
+  onSearch: (searchTerm: string) => void;
+}
+
+const SearchInput = ({ placeholder, onSearch }: SearchInputProps) => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      onSearch(searchTerm);
+      console.log(searchTerm);
+    }
+  };
+
+  return (
+    <Container>
+      <IconContainer>
+        <FontAwesomeIcon icon={faSearch} 
+        style={{ color: "#ccc" }}
+        />
+      </IconContainer>
+      <StyledInput
+        type="text"
+        placeholder={placeholder}
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        onKeyDown={handleKeyDown}
+      />
+    </Container>
+  );
+};
+
+export default SearchInput;
