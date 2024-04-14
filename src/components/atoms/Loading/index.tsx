b347a7cdf6b1da0ff85f8a3b92197503cce7b8ca
helenapaixao <hp.helenapaixao@gmail.com>
2024-04-13 21:55:46 -0300

    [feat] Create loading component with Container and Spinner

---
+++ src/components/atoms/Loading/index.tsx
@@ -0,0 +1,11 @@
+import { Container, Spinner } from "./styles";
+
+const Loading = () => {
+  return (
+    <Container data-testid="loading-component">
+      <Spinner data-testid="spinner-element" />
+    </Container>
+  );
+};
+
+export default Loading;
