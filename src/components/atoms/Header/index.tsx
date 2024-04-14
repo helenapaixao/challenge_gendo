b6de9fc8ae516f377b2d9bc8bb2cfd23b03c82b1
helenapaixao <hp.helenapaixao@gmail.com>
2024-04-13 21:54:17 -0300

    [feat] Implement Header component with FontAwesome for github logo

---
+++ src/components/atoms/Header/index.tsx
@@ -0,0 +1,29 @@
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Container, Subtitle, Title } from "./styles";
+
+export const Header = () => {
+  return (
+    <Container>
+      <FontAwesomeIcon
+        icon={faGithub}
+        data-testid="github-icon"
+        scale={1}
+        size={"2x"}
+        color={"#fff"}
+        style={{ marginRight: "10px", marginLeft: "20px" }}
+      />
+      <div
+        style={{
+          flexDirection: "row",
+          display: "flex",
+        }}
+      >
+        <Title>GitHub</Title>
+        <Subtitle>profiles</Subtitle>
+      </div>
+    </Container>
+  );
+};
+
+export default Header;
